51c64c3089e3a5c9c3f72c572106fc41accf4800
Wyatt Greene <techiferous@gmail.com>
2010-07-18 18:41:01 -0400

    Added multiple replacement test.

--- test/gsub_test.rb
+++ test/gsub_test.rb
@@ -51,4 +51,35 @@ class GsubTest < Test::Unit::TestCase
     assert_html_equal expected_html, after_html
   end
 
+  def test_multiple_replace
+    before_html = %Q{
+      <!DOCTYPE html
+      PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+        <head>
+          <title>Testing Rack::Gsub</title>
+        </head>
+        <body>
+          In Soviet Russia, there is censorship.
+        </body>
+      </html>
+    }
+    expected_html = %Q{
+      <!DOCTYPE html
+      PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+      <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+        <head>
+          <title>Testing Rack::Gsub</title>
+        </head>
+        <body>
+          In SovXXt RussXa, thXrX Xs cXnsorshXp.
+        </body>
+      </html>
+    }
+    after_html = process_html(before_html, Rack::Gsub, "i" => "X", "e" => "X")
+    assert_html_equal expected_html, after_html
+  end
+
 end
